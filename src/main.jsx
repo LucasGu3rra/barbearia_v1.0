dd09c4b797887829bf3971d88cc832797200e9fd
Lucas Guerra <lucasmaranhao29784@gmail.com>
2026-06-10 16:12:56 -0300

    slug fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import { atualizarManifestPwa } from './services/pwaManifest.js';
 import { registrarServiceWorker } from './services/serviceWorkerRegistration.js';
 import './index.css';
 
+atualizarManifestPwa();
 registrarServiceWorker();
 
 createRoot(document.getElementById('root')).render(
